bb33fc672e906f66a9f9f94880f28d43f70d7068
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-10-26 21:31:32 +0545

    add slices.go

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	l "github.com/atulsubedi/go-by-example/1-loop"
 )
 
-func main() {
-	// fmt.Println(l.Name)
+func main(){
 	fmt.Println(l.Loop())
 	fmt.Println(IfElse())
 	fmt.Println(Switch())
 	fmt.Println(Array())
+	fmt.Println(slices())
 }
---
+++ slices.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func slices() int {
+	var highScore = [3]int{1, 2, 3}
+	fmt.Println(highScore)
+	return 0
+}
