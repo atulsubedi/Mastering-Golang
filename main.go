39fe30bef50c2bee8784690162daebf4989e8517
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-10-28 21:07:16 +0545

    purefunc deleted

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 
-	//  l "github.com/atulsubedi/go-by-example/1-loop"
+	  l "github.com/atulsubedi/go-by-example/1-loop"
 )
 
 func main() {
-	//   fmt.Println(l.Loop())
-	//   fmt.Println(Ifelse())
-	//   fmt.Println(Slices())
-	//   fmt.Println(Array())
-	//   fmt.Println(Switch())
+	   fmt.Println(l.Loop())
+	   fmt.Println(Ifelse())
+	   fmt.Println(Slices())
+	   fmt.Println(Array())
+	   fmt.Println(Switch())
 
-	//   atul := Purefunc("atul","bharatpur",100,15)
-	//   fmt.Println(atul)
-	//   fmt.Println(Slices())
-	//   fmt.Println(Maps())	
+	   fmt.Println(atul)
+	   fmt.Println(Slices())
+	   fmt.Println(Maps())	
 	fmt.Println(Range())
 }
