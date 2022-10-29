3ab8e1c19efae278c0181eb2de009b6db510f972
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-10-29 08:01:53 +0545

    functions added

---
+++ functions.go
@@ -0,0 +1,33 @@
+// it is a package declaration we can read the code from here
+package main
+
+// this statement import the go package fmt  
+  import "fmt"  
+
+//   it is a function for simple addition of two digits
+func plus(a int, b int) int {
+	return a + b
+}
+
+// it is a function for addition of three digits
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+// the func Functions is the entry point for the go code 
+// code written here are only executable
+func Functions() int  {
+
+	// the func plus is called here 
+	res := plus(1, 2)
+	// it gives the result of the called func plus
+	fmt.Printf("result is:%d\n", res)
+
+	// the func plusPlus is called here 
+	res2 := plusPlus(2,3,4)
+		
+	// it gives the result of the called func plusPlus
+	fmt.Printf("result is:%d\n",res2)
+
+	return 0
+}
--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"fmt"
 
-	  l "github.com/atulsubedi/go-by-example/1-loop"
+	 l "github.com/atulsubedi/go-by-example/1-loop"
 )
 
 func main() {
-	   fmt.Println(l.Loop())
-	   fmt.Println(Ifelse())
-	   fmt.Println(Slices())
-	   fmt.Println(Array())
-	   fmt.Println(Switch())
+	 fmt.Println(l.Loop())
+	 fmt.Println(Ifelse())
+	 fmt.Println(Slices())
+	 fmt.Println(Array())
+	 fmt.Println(Switch())
 
-	   fmt.Println(atul)
-	   fmt.Println(Slices())
-	   fmt.Println(Maps())	
-	fmt.Println(Range())
+	 fmt.Println(Slices())
+	 fmt.Println(Maps())
+	 fmt.Println(Range())
+	fmt.Println(Functions())
 }
