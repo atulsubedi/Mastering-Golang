e0db896b0fbf4d93885ccb4ad2468b50a69f47ab
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-10-29 09:00:36 +0545

    variadic function added

--- main.go
+++ main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 
-	l "github.com/atulsubedi/go-by-example/1-loop"
+	// l "github.com/atulsubedi/go-by-example/1-loop"
 )
 
 func main() {
-	fmt.Println(l.Loop())
-	fmt.Println(Ifelse())
-	fmt.Println(Slices())
-	fmt.Println(Array())
-	fmt.Println(Switch())
-	fmt.Println(Slices())
-	fmt.Println(Maps())
-	fmt.Println(Range())
-	fmt.Println(Functions())
-	fmt.Println(multiValue())
+	// fmt.Println(l.Loop())
+	// fmt.Println(Ifelse())
+	// fmt.Println(Slices())
+	// fmt.Println(Array())
+	// fmt.Println(Switch())
+	// fmt.Println(Slices())
+	// fmt.Println(Maps())
+	// fmt.Println(Range())
+	// fmt.Println(Functions())
+	// fmt.Println(multiValue())
+	fmt.Println(Variadic())
+
 }
---
+++ variadic-function.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Multi(nums...int)int {
+	total := 1
+	for _, num := range nums{
+		// total = total * num
+		total *= num
+	}
+	return total	
+}
+
+func Variadic ()int{
+	fmt.Println(Multi(1,2,3,4))	
+	
+	// variadic function can be also used in slices
+
+	slice := []int{2,3,4,5}
+	fmt.Println(Multi(slice...))
+	return	0
+}
