8e35449fcc68ee0e082fe9c77f6b1474ceb99be8
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-10-29 13:35:23 +0545

    recursion added

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
-// it is a package declaration we can read code starting from here 
+// it is a package declaration, we can read code starting from here 
 package main
 
 // it import the go package fmt 
 import (
 	"fmt"
 
-	 l "github.com/atulsubedi/go-by-example/1-loop"
+	  l "github.com/atulsubedi/go-by-example/1-loop"
 )
 
 // it is the entry point for go code , the codes which are written here are only executable
 func main() {
-	 fmt.Println(l.Loop())
-	 fmt.Println(Ifelse())
-	 fmt.Println(Slices())
-	 fmt.Println(Array())
-	 fmt.Println(Switch())
-	 fmt.Println(Slices())
-	 fmt.Println(Maps())
-	 fmt.Println(Range())
-	 fmt.Println(Functions())
-	 fmt.Println(multiValue())
-	 fmt.Println(Variadic())
-	 fmt.Println(closures())
-
+	  fmt.Println(l.Loop())
+	  fmt.Println(Ifelse())
+	  fmt.Println(Slices())
+	  fmt.Println(Array())
+	  fmt.Println(Switch())
+	  fmt.Println(Slices())
+	  fmt.Println(Maps())
+	  fmt.Println(Range())
+	  fmt.Println(Functions())
+	  fmt.Println(multiValue())
+	  fmt.Println(Variadic())
+	  fmt.Println(closures())
+	  fmt.Println(recursion())
 }
